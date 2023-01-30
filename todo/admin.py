0b20bb9882df9731ebc7d7cfa77d4fa45349e4cd
KeoCode <keocode101@gmail.com>
2023-01-30 15:33:51 +0000

    Prepared to deploy to herokiu

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import item
+from .models import Item
 
 # Register your models here.
-admin.site.register(item)
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class item(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
