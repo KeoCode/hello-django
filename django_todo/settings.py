1bddf46db210277ba1fba69cb87effa6622c9272
KeoCode <keocode101@gmail.com>
2023-01-31 20:43:16 +0000

    Added blank line to Procfile

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ehw5s5vgi*9*8xbh*&hu*q0#$sua38%*zu3q6g=08*&xfqvzs'
+SECRET_KEY = os.environ.get('SECRET_KEY', '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
